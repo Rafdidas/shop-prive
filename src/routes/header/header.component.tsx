f1a331adb355663c9a251fa446b15f338df03547
HyeonGyu <yhu930421@naver.com>
2025-01-15 00:06:10 +0900

    반응형 완료 트러블 슈팅 중

--- src/routes/header/header.component.tsx
+++ src/routes/header/header.component.tsx
@@ -1,6 +1,7 @@
 import './header.styles.scss';
+import './lnb.styles.scss';
 
-import { FC, Fragment, useEffect } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { RootState, AppDispatch } from '../../store/store';
@@ -10,6 +11,7 @@ import { fetchCategories } from '../../store/categories/categories.action';
 import { logoutUser } from '../../store/user/user.slice';
 
 const Header: FC = () => {
+    const [lnbOpen, setLnbOpen] = useState(false);
     const dispatch: AppDispatch = useDispatch();
     
     const currentUser = useSelector((state: RootState) => state.user.currentUser);
@@ -22,18 +24,26 @@ const Header: FC = () => {
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error: {error}</p>
-    
-    console.log("Current User in Header:", currentUser);
 
     const handleLogout = () => {
         dispatch(logoutUser());
     };
 
+    const toggleLnb = () => {
+        setLnbOpen((prevState) => !prevState);
+    };
+
+    const closeLnb = () => {
+        setLnbOpen(false); // LNB를 닫음
+    };
+
     return (
+        <Fragment>
         <header>
             <div className='gnb'>
                 <div className='wrap_inner3'>
                     <div className='hd_left_menu'>
+                        <div className="ham" onClick={toggleLnb}></div>
                         <h1 className='logo'><Link to='/'>PRIVE</Link></h1>
                         <ul className='hd_menu_list'>
                             <li><Link to='/shop'>All</Link></li>
@@ -62,6 +72,33 @@ const Header: FC = () => {
                 </div>
             </div>
         </header>
+
+        <nav className={`lnb_wrap ${lnbOpen ? 'on' : ''}`}>
+            <div className='lnb'>
+                <div className='wrap_inner3'>
+                    <div className="top_lnb">
+					    <div className="ham" onClick={toggleLnb}></div>
+					    <h1 className='logo'><Link to='/' onClick={closeLnb}>PRIVE</Link></h1>
+                        {currentUser ? (
+                            <p className='user_lnb'>반갑습니다. {currentUser.displayName || currentUser.email} 님</p>
+                        ) : (
+                            <p className='user_lnb'>반갑습니다.</p>
+                        )}
+				    </div>
+                    <ul className='hd_menu_list'>
+                        <li><Link to='/shop' onClick={closeLnb}>All</Link></li>
+                        {Array.isArray(categories) && categories.map((category) => {
+                            return (
+                                <li key={category}>
+                                    <Link to={`/shop/${category}`} onClick={closeLnb}>{category}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </Fragment>
     )
 }
 
